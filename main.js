28ae63a8e84d287154890b9bba09dabf9a11312f
luistalero <luisalbertotaleromartinez@gmail.com>
2024-09-18 16:59:39 -0500

    feat: commit 3

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import root from "./componentes/root.js";
 import titulo from "./componentes/titulo.js";
 import div2 from "./componentes/contenedorTareas.js";
 import tareaContenedor from "./componentes/crearInput.js";
+import Tarea from "./clases/tarea.js";
+
+const miTarea = new Tarea("hola mundo");
+
+miTarea.texto = ""
+root.append(miTarea.etiqueta);
 
 root.append(titulo);
 root.append(tareaContenedor);
